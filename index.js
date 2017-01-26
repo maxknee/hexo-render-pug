9036c801ce5540e02ba02af9538588354d70ece5
Maciej Krzemianowski <git@easiestsolution.eu>
2017-01-26 21:17:53 +0100

    Fixed spacings.

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var pugRenderer = require('./lib/renderer');
 var extend = require('util')._extend;
 
 var userConfig = {
-    options: hexo.config.pug || {}
+  options: hexo.config.pug || {}
 };
 
 hexo.extend.renderer.register('pug', 'html', function(data, locals) {
-    return pugRenderer(extend(data, userConfig), locals);
+  return pugRenderer(extend(data, userConfig), locals);
 }, true);
